8605147338d7a4f3a11da7b1d0368750f071065a
Kimsuro <ss01056489656@gmail.com>
2025-11-02 11:21:21 +0900

    feat(constants): 로또 당첨 등수별 상금 상수 추가
    - 1등~5등 당첨 조건 및 상금 데이터 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,4 +6,37 @@ const LOTTO_CONFIG = Object.freeze({
   SEPERATOR: ',',
 });
 
+export const LOTTO_PRIZES = Object.freeze([
+  Object.freeze({
+    rank: 'FIRST',
+    match: 6,
+    hasBonus: false,
+    reward: 2_000_000_000,
+  }),
+  Object.freeze({
+    rank: 'SECOND',
+    match: 5,
+    hasBonus: true,
+    reward: 30_000_000,
+  }),
+  Object.freeze({
+    rank: 'THIRD',
+    match: 5,
+    hasBonus: false,
+    reward: 1_500_000,
+  }),
+  Object.freeze({
+    rank: 'FOURTH',
+    match: 4,
+    hasBonus: false,
+    reward: 50_000,
+  }),
+  Object.freeze({
+    rank: 'FIFTH',
+    match: 3,
+    hasBonus: false,
+    reward: 5_000,
+  }),
+]);
+
 export default LOTTO_CONFIG;
