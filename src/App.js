dd6856e6d8f9c4574bce37d0e49966254af593ed
Kimsuro <ss01056489656@gmail.com>
2025-11-03 19:48:52 +0900

    refactor(app): 입력 계층 검증 로직 개선
    - Validator 클래스에서 validators 함수 사용
    - 각 입력 메서드에서 검증 수행

--- src/App.js
+++ src/App.js
@@ -1,72 +1,90 @@
 import { Console } from '@woowacourse/mission-utils';
-import { LOTTO_CONFIG } from './constants/constants.js';
 import InputView from './view/InputView.js';
-import OutputView from './view/OutputView.js';
+import {
+  validateBonusNumber,
+  validateCommon,
+  validateLotto,
+  validateWinningNumbers,
+} from './utils/validators.js';
+import { parseStringToNumber, parseStringToNumberArray } from './utils/inputParser.js';
 import LottoMachine from './service/LottoMachine.js';
-import WinningLotto from './service/WinningLotto.js';
+import OutputView from './view/OutputView.js';
 import LottoResult from './service/LottoResult.js';
-import Lotto from './model/Lotto.js';
-import Validator from './validator/Validator.js';
+import WinningLotto from './service/WinningLotto.js';
 
 class App {
   async run() {
-    const { money, lottos } = await this.#purchaseLottos();
+    const { money, lottos } = await this.#getPurchaseLottos();
     const winningLotto = await this.#getWinningLotto();
     this.#calculateAndPrintResult(lottos, winningLotto, money);
   }
 
-  async #purchaseLottos() {
-    try {
-      const input = await InputView.readUserMoney();
-      this.#validateCommonInput(input);
+  async #readUserMoney() {
+    while (true) {
+      try {
+        const input = await InputView.readUserMoney();
+        validateCommon(input);
 
-      const money = Number(input);
-      const lottoMachine = new LottoMachine(money);
-      const lottos = lottoMachine.generateLottos();
+        return parseStringToNumber(input);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
 
-      OutputView.printPurchasedLottos(lottos);
+  async #readWinningNumbers() {
+    while (true) {
+      try {
+        const input = await InputView.readWinningNumbers();
+        validateWinningNumbers(input);
 
-      return { money, lottos };
-    } catch (error) {
-      Console.print(error.message);
-      return this.#purchaseLottos();
+        const numbers = parseStringToNumberArray(input);
+        validateLotto(numbers);
+
+        return numbers;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  async #readBonusNumber(winningNumbers) {
+    while (true) {
+      try {
+        const input = await InputView.readBonusNumber();
+        validateCommon(input);
+
+        const bonusNumber = parseStringToNumber(input);
+        validateBonusNumber(bonusNumber, winningNumbers);
+
+        return bonusNumber;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  async #getPurchaseLottos() {
+    while (true) {
+      try {
+        const money = await this.#readUserMoney();
+        const lottoMachine = new LottoMachine(money);
+        const lottos = lottoMachine.generateLottos();
+
+        OutputView.printPurchasedLottos(lottos);
+
+        return { money, lottos };
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
   }
 
   async #getWinningLotto() {
-    const winningNumbers = await this.#getWinningNumbers();
-    return this.#createWinningLotto(winningNumbers);
-  }
+    const winningNumbers = await this.#readWinningNumbers();
+    const bonusNumber = await this.#readBonusNumber(winningNumbers);
 
-  async #createWinningLotto(winningNumbers) {
-    try {
-      const bonusNumber = await this.#getBonusNumber();
-      return new WinningLotto(winningNumbers, bonusNumber);
-    } catch (error) {
-      Console.print(error.message);
-      return this.#createWinningLotto(winningNumbers);
-    }
-  }
-
-  async #getWinningNumbers() {
-    try {
-      const input = await InputView.readWinningNumbers();
-      Validator.validateEmpty(input);
-      Validator.validateHasCommas(input);
-
-      const numbers = this.#parseWinningNumbers(input);
-      Lotto.validateNumbers(numbers);
-      return numbers;
-    } catch (error) {
-      Console.print(error.message);
-      return this.#getWinningNumbers();
-    }
-  }
-
-  async #getBonusNumber() {
-    const input = await InputView.readBonusNumber();
-    this.#validateCommonInput(input);
-    return Number(input);
+    return new WinningLotto(winningNumbers, bonusNumber);
   }
 
   #calculateAndPrintResult(lottos, winningLotto, purchaseAmount) {
@@ -77,18 +95,6 @@ class App {
     });
 
     OutputView.printResult(result, purchaseAmount);
-  }
-
-  #validateCommonInput(input) {
-    Validator.validateEmpty(input);
-
-    const number = Number(input);
-    Validator.validateNumber(number);
-    Validator.validateInteger(number);
-  }
-
-  #parseWinningNumbers(input) {
-    return input.split(LOTTO_CONFIG.SEPARATOR).map((num) => Number(num.trim()));
   }
 }
 
